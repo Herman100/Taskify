957fb001825ff1a23ba1a7e4ed9eff9c44f0c8d7
Herman100 <hermankwamebour30@gmail.com>
2024-03-01 01:56:30 +0000

    header component

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import taskifylogo from "./assets/taskifylogo.svg";
+import taskifylogoicon from "./assets/taskifylogoicon.svg";
+
+export default function Logo({ theme, collapsed }) {
+  const navigate = useNavigate();
+  function handleLogoClick() {
+    navigate("/Taskify/");
+  }
+
+  const styles = {
+    height: "75px",
+    margin: "10px 0",
+    width: "100%",
+    filter:
+      theme === "dark"
+        ? "invert(100%) sepia(100%) saturate(0%) hue-rotate(78deg) brightness(101%) contrast(103%)"
+        : "invert(0%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%)",
+
+    // backgroundColor: theme === "dark" ? "white " : "#150654",
+  };
+  return (
+    <div>
+      {collapsed ? (
+        <img
+          src={taskifylogoicon}
+          alt="taskify logo"
+          style={styles}
+          onClick={handleLogoClick}
+        />
+      ) : (
+        <img
+          src={taskifylogo}
+          alt="taskify logo"
+          style={styles}
+          onClick={handleLogoClick}
+        />
+      )}
+    </div>
+  );
+}
